6518272a5d68f85e7e578141f805e73fe23b38ba
alexis.drai <alexis.drai@etu.uca.fr>
2023-01-27 11:41:36 +0100

    Yeah, my little pig 👱‍♀️

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <algorithm>
 #include "Personnage/Personnage.h"
 #include "Personnage/Kenny.h"
 
@@ -26,5 +27,45 @@ int main()
     second->parler("I'm here too");
     // that sucked
 
+    cout << endl;
+    anime.insert(next(anime.begin()), new Personnage());
+
+    for (Personnage *character: anime)
+    {
+        character->parler("Hey dude.");
+    }
+
+    cout << endl;
+    vector<Personnage *> vanime;
+
+    for (Personnage *character: vanime)
+    {
+        character->parler("Hey dude.");
+    }
+
+    cout << endl;
+    copy(anime.begin(), anime.end(), back_inserter(vanime));
+
+
+    for (Personnage *character: vanime)
+    {
+        character->parler("Hey dude.");
+    }
+
+    cout << endl;
+    vanime[2]->parler("I'm third");
+    vanime.insert(next(vanime.begin()), new Personnage());
+    // same as with the list...
+
+    cout << endl;
+    for (Personnage *character: vanime)
+    {
+        character->parler("Hey dude.");
+    }
+
+
+    // we copied pointers in vanime, we didn't call 'new()' -- no need to delete from both collections
+    for_each(vanime.begin(), vanime.end(), [](auto *x)
+    { delete x; });
     return 0;
 }
